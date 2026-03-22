fcc781e5c70013f999c53e0e1c350bd71053956c
Pushparaj Singh Parmar <psinghparmar42@gmail.com>
2026-03-22 18:13:18 +0530

    stack

---
+++ DSA/Stacks/InbuiltStack.java
@@ -0,0 +1,24 @@
+
+import java.util.Stack;
+
+public class InbuiltStack{
+
+public static void main (String[]args){
+Stack<Integer>stack=new Stack<>();
+stack.push(34);
+stack.push(3);
+stack.push(4);
+stack.push(444);
+stack.push(112);
+stack.push(86);
+stack.push(34675);
+System.out.println(stack.pop());
+System.out.println(stack.pop());
+System.out.println(stack.pop());
+System.out.println(stack.pop());
+System.out.println(stack.pop());
+System.out.println(stack.pop());
+
+
+}
+}
